ebb01df1d5d63ae25a6449e2ed43e7d005789022
arthurclecio <127309519+arthurclecio@users.noreply.github.com>
2023-08-07 09:58:14 -0300

    Add files via upload

---
+++ mensagem.js
@@ -0,0 +1,8 @@
+  const botao = document.querySelector("#meuBotao");
+  const elemento = document.querySelector("#meuElemento");
+
+  botao.addEventListener("click", function() {
+
+    elemento.innerText = "feliz seu dia alvaro";
+
+  });
